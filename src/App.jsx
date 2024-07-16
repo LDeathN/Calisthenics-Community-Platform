b8d37558527fb700a3561de46caa1337db0b2dac
Martin <martyndaskalov2004@gmail.com>
2024-07-16 20:23:43 +0300

    Starting the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import WorkoutCatalog from './components/WorkoutCatalog';
-import WorkoutDetails from './components/WorkoutDetails';
-import CreateWorkout from './components/CreateWorkout';
-import EditWorkout from './components/EditWorkout';
-import DeleteWorkout from './components/DeleteWorkout';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
-import PrivateRoute from './components/PrivateRoute';
-import PublicRoute from './components/PublicRoute';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="main">
-        <Navigation />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/workouts" element={<WorkoutCatalog />} />
-          <Route path="/workouts/:id" element={<WorkoutDetails />} />
-          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/create-workout" element={<PrivateRoute><CreateWorkout /></PrivateRoute>} />
-          <Route path="/edit-workout/:id" element={<PrivateRoute><EditWorkout /></PrivateRoute>} />
-          <Route path="/delete-workout/:id" element={<PrivateRoute><DeleteWorkout /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/*" element={<Navigate to="/" />} />
-        </Routes>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
