7e030b13ed874bde4981f80de0820c2a42e010c1
Martin <martyndaskalov2004@gmail.com>
2024-07-28 13:56:37 +0300

    initial commit

--- src/hooks/usePersistedState.js
+++ src/hooks/usePersistedState.js
@@ -20,8 +20,12 @@ export default function usePersistedState(key, initialState) {
         ? value(state)
         : value;
 
-        localStorage.setItem(key, JSON.stringify(newState));
-        
+        if (newState === null || newState === undefined) {
+            return localStorage.removeItem(key);
+        } else {
+            localStorage.setItem(key, JSON.stringify(newState));
+        }
+
         setState(newState);
     }
 
